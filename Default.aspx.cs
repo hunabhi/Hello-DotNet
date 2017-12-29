7b1e09a5602534d671e4cf9e11e33599465f3932
Abhishek <er.abhishektomar@gmail.com>
2017-12-30 04:01:40 +0530

    added button code

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -17,5 +17,6 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
+        TextBox3.Text = (Convert.ToInt16(TextBox2.Text) + Convert.ToInt16(TextBox1.Text)).ToString();
     }
 }
